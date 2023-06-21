cc6c0c55628aec90348214dee514ae2d1bf6c115
Astra <me@astrra.space>
2023-06-21 13:41:50 +0300

    Build for all (for now)

--- conf.py
+++ conf.py
@@ -59,12 +59,10 @@ html_static_path = ['_static']
 smv_tag_whitelist = r'^.*$'
 
 # Whitelist pattern for branches (set to None to ignore all branches)
-# Regex to match the string dev
-smv_branch_whitelist = r'^(?!dev$).*$'
+smv_branch_whitelist = r'^.*$'
 
 # Whitelist pattern for remotes (set to None to use local branches only)
-# Regex to match the string dev
-smv_remote_whitelist = r'^(?!dev$).*$'
+smv_remote_whitelist = r'^.*$'
 
 # Pattern for released versions
 smv_released_pattern = r'^tags/.*$'
